4e5677389a124860d004487145a35386318335b8
Sirak93 <sirak.grigoryan.93@mail.ru>
2021-06-13 18:09:11 +0400

    first commit

---
+++ src/app/post4/post4.component.ts
@@ -0,0 +1,21 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'app-post4',
+  template: `
+    <div class="post4">
+      <h2>Post4 title</h2>
+
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae, tenetur!</p>
+    </div>
+  `,
+  styles: [`
+    .post4 {
+      color: blue;
+    }
+  `]
+})
+
+export class Post4Component {
+
+}
